42e2d9d9a84572736bcb2fc5027f3c4605e5e312
Okjit <77417233+Okjit@users.noreply.github.com>
2021-01-14 16:23:42 -0500

    Update Nuker.py

--- choppaWW-main/choppaWW-main/Ezo-Nuker-main/Nuker.py
+++ choppaWW-main/choppaWW-main/Ezo-Nuker-main/Nuker.py
@@ -15,7 +15,7 @@ Clear()
 
 while True:
     password = input("[+] Enter Password: ")
-    if password =="EzoW":
+    if password =="VloneX":
         print("Correct password")
         input("[+] enter to continue to Selfbot")
         break
@@ -35,7 +35,7 @@ while True:
 Clear()
 
 
-token = input("Enter token noob : ")
+token = input("Enter token jit : ")
 
 head = {'Authorization': str(token)}
 src = requests.get('https://discordapp.com/api/v6/users/@me', headers=head)
@@ -44,20 +44,20 @@ if src.status_code == 200:
     print('[+] Valid token')
     input("Press any key")
 else:
-    print(f'[{Fore.RED}-{Fore.RESET}] Wrong token tuff')
+    print(f'[{Fore.ORANGE}-{Fore.RESET}] Wrong token tuff')
     input("Press any key")
     exit(0)
 Clear()
 
 
-print(f'''{Fore.RED}
-                                     ______ __________   __          __
-                                     |  ____|___  / __ \  \ \        / /
-                                     | |__     / / |  | |  \ \  /\  / / 
-                                     |  __|   / /| |  | |   \ \/  \/ /  
-                                     | |____ / /__ |__| |    \  /\  /   
-                                     |______/_____\____/      \/  \/    
-                                    
+print(f'''{Fore.ORANGE}
+                                   __      ___      ____  _   _ ________   __  _   _ _    _ _  ________ _____  
+                                   \ \    / / |    / __ \| \ | |  ____\ \ / / | \ | | |  | | |/ /  ____|  __ \ 
+                                    \ \  / /| |   | |  | |  \| | |__   \ V /  |  \| | |  | | ' /| |__  | |__) |
+                                     \ \/ / | |   | |  | | . ` |  __|   > <   | . ` | |  | |  < |  __| |  _  / 
+                                      \  /  | |____ |__| | |\  | |____ / . \  | |\  | |__| | . \| |____| | \ \ 
+                                       \/   |______\____/|_| \_|______/_/ \_\ |_| \_|\____/|_|\_\______|_|  \_\
+                                                                             
 
                                                                                                        ''')
 
@@ -68,11 +68,11 @@ client = discord.Client()
 async def on_connect():
   for user in client.user.friends:
     try:
-      Ezo = discord.Embed(color= discord.Color(0xDBC415))
-      Ezo.set_author(name="Nuker by Ezo")
-      Ezo.add_field(name="Ezo nuker",value=Text)
-      Ezo.set_image(url="https://media.discordapp.net/attachments/676163687869841418/799136057757138975/image0.gif?width=422&height=422")
-      await user.send(embed=Ezo)
+      vlonex = discord.Embed(color= discord.Color(0x4146d1))
+      vlonex.set_author(name="Nuker by VloneX")
+      vlonex.add_field(name="VloneX nuker",value=Text)
+      vlonex.set_image(url="")
+      await user.send(embed=vlonex)
       print(f"messaged: {user.name}")
     except:
        print(f"couldnt message: {user.name}")
